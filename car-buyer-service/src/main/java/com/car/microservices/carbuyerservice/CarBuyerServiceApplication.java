e05685e924e3a73c5b5072b7f36b1e74d6b98440
Daemian <daemian.koh@gmail.com>
2022-07-16 23:45:59 +0800

    Add Car Buyer Repo

---
+++ car-buyer-service/src/main/java/com/car/microservices/carbuyerservice/CarBuyerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.car.microservices.carbuyerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBuyerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBuyerServiceApplication.class, args);
+	}
+
+}
---
+++ car-buyer-service/src/test/java/com/car/microservices/carbuyerservice/CarBuyerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.microservices.carbuyerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBuyerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
