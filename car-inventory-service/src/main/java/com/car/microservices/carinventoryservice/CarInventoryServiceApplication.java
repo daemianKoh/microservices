4ec6fdf3b331df826fb9d0b5f71a09bb3e44a254
Daemian <daemian.koh@gmail.com>
2022-07-16 22:00:46 +0800

    car inventory

---
+++ car-inventory-service/src/main/java/com/car/microservices/carinventoryservice/CarInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.car.microservices.carinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInventoryServiceApplication.class, args);
+	}
+
+}
