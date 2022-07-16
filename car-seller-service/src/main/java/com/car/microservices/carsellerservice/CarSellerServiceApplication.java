996cbac64132c740d6f29293262e7fc14d79b67b
Daemian <daemian.koh@gmail.com>
2022-07-16 23:45:36 +0800

    Add car seller repo

---
+++ car-seller-service/src/main/java/com/car/microservices/carsellerservice/CarSellerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.car.microservices.carsellerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSellerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSellerServiceApplication.class, args);
+	}
+
+}
---
+++ car-seller-service/src/test/java/com/car/microservices/carsellerservice/CarSellerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.microservices.carsellerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSellerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
